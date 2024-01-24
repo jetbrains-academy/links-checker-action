0bba274ab982e67f800f2def2ea520c178f983ba
Konstantin Chaika <10850581+kochaika@users.noreply.github.com>
2024-01-24 15:10:07 +0400

    Update headers to prevent 403 error

--- main.py
+++ main.py
@@ -52,7 +52,13 @@ def check_link(task_folder, link):
             else:
                 return True, ""
         elif link.startswith(('http://', 'https://')):
-            headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
+            headers = {
+                'authority': 'www.google.com',
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'accept-language': 'en-US,en;q=0.9',
+                'cache-control': 'max-age=0',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+            }
             response = requests.head(link, headers=headers)
             if 400 <= response.status_code <= 599:
                 return False, f"NOT valid url (returns {response.status_code}): {link}"
